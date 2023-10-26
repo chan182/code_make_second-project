4dfb26474002ef875e474b13c141f394138ed200
yeonna18k <yeonna18k@gmail.com>
2023-10-26 10:37:12 +0900

    pagination

--- js/main.js
+++ js/main.js
@@ -58,7 +58,81 @@ function makePagination(currentPage, totalPages) {
   // loop 마지막 번호
   const loopEndNum = currentPageGroup * pageGroupLimit;
 
-  for (let i = loopStartNum; i <= loopEndNum; i++) {}
+  const pageCard = document.createElement("div");
+  pageCard.innerHTML += `<button>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 0 512 512"
+  >
+    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+    <path
+      fill="currentColor"
+      d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z"
+    ></path>
+  </svg>
+</button>
+<button>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 0 320 512"
+  >
+    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+    <path
+      fill="currentColor"
+      d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
+    ></path>
+  </svg>
+</button>`;
+  for (let i = loopStartNum; i <= loopEndNum; i++) {
+    // console.log(i);
+    // console.log(`<button>${i}</button>`);
+    pageCard.innerHTML += `<button class="btn-num">${i}</button>`;
+  }
+  pageCard.innerHTML += `<button>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 0 320 512"
+  >
+    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+    <path
+      fill="currentColor"
+      d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+    ></path>
+  </svg>
+</button>
+<button>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 0 512 512"
+  >
+    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+    <path
+      fill="currentColor"
+      d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z"
+    ></path>
+  </svg>
+</button>`;
+  console.log(pageCard);
+
+  let paginationWrapper = document.querySelector(".pagination-wrapper");
+  paginationWrapper.append(pageCard);
+
+  const btn = document.querySelector("btn-num");
+  const active = ".active-pagination";
+  btn.classList.add(active);
+  handlePageClick(loopStartNum, loopEndNum);
+}
+
+function handlePageClick(loopStartNum, loopEndNum) {
+  for (let i = loopStartNum; i <= loopEndNum; i++) {
+    const btnNum = document.querySelector(`.btn_num${i}`);
+    const active = document.querySelector(".active-pagination");
+    btnNum.classList.add(active);
+  }
 }
 
 ///  2. 검색 기능 ( get 요청하여 검색하여 정보를 가지고 옴)
