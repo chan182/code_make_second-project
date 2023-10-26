c14634ce525bdb40b15760c6282bb151013dad8e
hc182 <khch503@naver.com>
2023-10-26 20:02:24 +0900

    heechan commit

--- sub.js
+++ sub.js
@@ -1,47 +1,79 @@
-let inputName = document.getElementById("input-name");
+const inputName = document.getElementById("input-name");
+const inputContent = document.getElementById("input-content");
+const inputPassword = document.getElementById("input-password");
+const deleteButton = document.getElementById("delete-button");
+const cardList = document.getElementById("review-list");
+const INFO_KEY = "infos";
+
+let inFos = [];
+
 let buttonClick = document.getElementById("button-click");
 
 buttonClick.addEventListener("click", play);
 
+// 저장하기 3
+function saveInfos(newInfos) {
+  localStorage.setItem(INFO_KEY, JSON.stringify(newInfos));
+}
+
+// 저장하기 1
 function play() {
-  nameContent = inputName.value;
-  localStorage.setItem("nickname", nameContent);
-  render();
+  const newName = inputName.value;
+  const newContent = inputContent.value;
+  const newPassword = inputPassword.value;
+  const newInfoObj = {
+    name: newName,
+    content: newContent,
+    password: newPassword,
+    id: Date.now(),
+  };
+  inFos.push(newInfoObj);
+  render(newInfoObj);
+  saveInfos(inFos);
 }
-function render() {
-  let name = localStorage.getItem("nickname");
-  console.log(name);
-  let resultHTML = `<div class="cards">
-<div id="result-nickname">
-    닉네임 : ${name}
-</div>
-<div>
-    한줄평 : 너무 재미있어요
-</div>
-<div>
-    확인 비밀번호 : 1234
-</div>
-<div class="input-control">
 
-    <button>수정</button>
-    <button>삭제</button>
-</div>
-</div>`;
+// 삭제
 
-  document.getElementsByClassName("review-list").innerHTML = resultHTML;
+function deleteInfos(event) {
+  let newPAssword = prompt();
+  const div0 = event.target.parentElement;
+  div0.remove();
+  const filterInFos = inFos.filter((info) => info.id !== parseInt(div0.id));
+  saveInfos(filterInFos);
 }
-// function render() {
-//   const name = localStorage.getItem("nikname");
-//   console.log(name);
-//   let resultHTML = `<div class="cards">
-//   <div id="result-nickname">
-//       닉네임 : ${name}
-//   </div>
-//   <div>
-//       한줄평 : 너무 재미있어요
-//   </div>
-//   <div>
-//       확인 비밀번호 : 1234
-//   </div>
-// </div>`;
+
+// 수정
+
+// function modifyInfos(event) {
+//   const div = event.target.parentElement;
 // }
+
+// 2. 가져오기 및 그려주기
+
+function render(newInfoObj) {
+  const div0 = document.createElement("div");
+  div0.id = newInfoObj.id;
+  const divName = document.createElement("div");
+  divName.innerText = `이름 : ${newInfoObj.name}`;
+  const divContent = document.createElement("div");
+  divContent.innerText = `한줄평 : ${newInfoObj.content}`;
+  const button1 = document.createElement("button");
+  button1.innerText = "삭제";
+  const button2 = document.createElement("button");
+  button2.innerText = "수정";
+  button1.addEventListener("click", deleteInfos);
+  // button2.addEventListener("click", modifyInfos);
+  div0.appendChild(divName);
+  div0.appendChild(divContent);
+  div0.appendChild(button1);
+  div0.appendChild(button2);
+  cardList.appendChild(div0);
+}
+
+const savedInfos = localStorage.getItem(INFO_KEY);
+
+if (savedInfos !== null) {
+  const parsedInfos = JSON.parse(savedInfos);
+  inFos = parsedInfos;
+  parsedInfos.forEach(render);
+}
