be929f8801b0077220a6f16e21f03b304590030e
yeolsss <yeols8810@gmail.com>
2023-10-27 14:24:21 +0900

    1차 정리 완료

--- js/detail.js
+++ js/detail.js
@@ -1,15 +1,11 @@
-//  영화 세부사항
+import { makeRatingCircle } from "./util.js";
 
-//현재 선택한 영화의 ID 값 + 페이지 값 받기
-// console.log('현재 url의 파라미터 값'+window``.location.search)
-// const url =window.location.href;
-// const urlParams=url.searchParams;
-// const movieId=urlParams.get('movie-id');
-// const page=urlParams.get('페이지값');
+//  영화 세부사항
 
 const url = window.location.href;
 const path = new URL(url);
 const movieId = path.searchParams.get("movie_id");
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 // const currntPage = path.searchParams.get("페이지");
 
 // aysnc await 으로 받아오면 되고
@@ -25,14 +21,39 @@ const options = {
 fetch(`https://api.themoviedb.org/3/movie/${movieId}?language=ko-KR`, options)
   .then((response) => response.json())
   .then((response) => {
-    const { title, backdrop_path, overview, release_date, runtime } = response;
+    const {
+      title,
+      backdrop_path,
+      overview,
+      release_date,
+      runtime,
+      belongs_to_collection,
+      genres,
+      vote_average,
+      tagline,
+    } = response;
+
+    document.title = title;
+
+    //  장르
+    const createGenres = genres.map((item) => {
+      return item.name;
+    });
+
+    // review wrapper imgage 변경
+    const reviewWrapper = document.querySelector(".review-wrapper");
+    reviewWrapper.style.backgroundImage = `url(${IMG_PATH}${backdrop_path})`;
 
     //nav bar
     let nav = document.querySelector("#navbar");
-    const navhtml = `<div class='nav'><img class='nav-img'src='https://image.tmdb.org/t/p/w300${backdrop_path}'></div>
-    <div class='nav'><span style='font-weight:800; font-size:20px;'> ${title}</span><br></br><br></br>
-    <span style='font-weight:700;'>개봉일:</span>  ${release_date}<br></br><br></br>
-    <span style='font-weight:700;'>상영시간:</span>  ${runtime} 분
+    const navhtml = `
+    <div class="nav">
+      <img class="nav-img" src="${IMG_PATH}${backdrop_path}">
+        ${makeRatingCircle(vote_average, "sticky-circle")}
+      </div>
+      <div class="nav"><span style="font-weight:800; font-size:20px;"> ${title}</span><br></br><br></br>
+      <span style="font-weight:700;">개봉일:</span>  ${release_date}<br></br><br></br>
+      <span style="font-weight:700;">상영시간:</span>  ${runtime} 분
     </div>
     `;
     nav.innerHTML = navhtml;
@@ -40,32 +61,45 @@ fetch(`https://api.themoviedb.org/3/movie/${movieId}?language=ko-KR`, options)
 
     // 상세페이지
     const detailMain = document.querySelector(".detail-main");
-    let text = ` 
-  <div class="item"><img class='detail-img'src='https://image.tmdb.org/t/p/w300${backdrop_path}'></div>
-  <div class="item title" style='border-bottom:1px solid black; 
-  font-size:30px; font-weight:700;'>제목:${title}  </div>
-  <div class="item">개봉일:${release_date} </div>
-  <div class="item">상영시간:${runtime}분  </div>
-  <div class="item">줄거리:${overview}  </div>`;
-    detailMain.innerHTML = text;
+    detailMain.innerHTML = ` 
+  <div class="item">
+  <img class="detail-img" src="${IMG_PATH}${backdrop_path}">
+  ${makeRatingCircle(vote_average, "detail")}
+  </div>
+  
+  <div class="item title" style="border-bottom:1px solid black; font-size:30px; font-weight:700;">${title}</div>
+  <div class="item">개봉일: ${release_date} </div>
+  <div class="item">상영시간: ${runtime}분  </div>
+  <div class="item">장르: ${createGenres}  </div>
+  `;
     // 상세페이지
 
-    document.querySelector(".content").append(`${overview}`);
+    document.querySelector(".content").innerHTML = `
+      <br/>
+      <h1 style="font-size:4rem; font-weight: bold;">${title}</h1>
+      <br/>
+      <br/>
+      <strong style="font-size:3.2rem; font-weight: bold;">${tagline}</strong>
+      <br/>
+      <br/>
+      ${overview === "" ? "줄거리가 없습니다." : overview}  `;
   })
   .catch((err) => console.error(err));
 
 //  Tab 생성 start
-const btnTab = document.querySelector(".list");
+const btnTab = document.querySelectorAll(".list > li");
 const tabContents = document.querySelectorAll(".tab-content");
-btnTab.addEventListener("click", function (e) {
-  for (let i = 0; i < e.currentTarget.children.length; i++) {
-    e.currentTarget.children[i].classList.remove("selected");
-    tabContents[i].classList.remove("show");
-    console.log("돌아돌아");
-  }
-  //이 for문으로 인해 select 된 태그를 초기화 시켜준다. -> 현재 보여지는 selected를 없애줌
-  e.target.classList.add("selected");
-  tabContents[e.target.dataset.id].classList.add("show");
+btnTab.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const parent = e.target.parentNode;
+    for (let i = 0; i < parent.children.length; i++) {
+      parent.children[i].classList.remove("selected");
+      tabContents[i].classList.remove("show");
+    }
+    //이 for문으로 인해 select 된 태그를 초기화 시켜준다. -> 현재 보여지는 selected를 없애줌
+    e.target.classList.add("selected");
+    tabContents[e.target.dataset.id].classList.add("show");
+  });
 });
 //  Tab 생성 end
 
@@ -78,23 +112,25 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (
-    document.body.scrollTop > 470 ||
-    document.documentElement.scrollTop > 470
+    document.body.scrollTop >= 445 ||
+    document.documentElement.scrollTop >= 445
   ) {
-    document.getElementById("navbar").style.top = "0px";
+    document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-200px";
+    document.getElementById("navbar").style.top = "-100%";
   }
 }
 
-let navbar = document.querySelector("ul.list");
-let sticky = navbar.offsetTop;
-
 function myFunction() {
+  const navbar = document.querySelector("ul.list");
+  const content = document.querySelector(".tab-content.show");
+  const sticky = navbar.offsetTop;
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
+    content.style.paddingTop = "390px";
   } else {
     navbar.classList.remove("sticky");
+    content.style.paddingTop = "0";
   }
 }
 
@@ -104,13 +140,12 @@ const inputName = document.getElementById("input-name");
 const inputContent = document.getElementById("input-content");
 const inputPassword = document.getElementById("input-password");
 const deleteButton = document.getElementById("delete-button");
-const cardList = document.getElementById("review-list");
+const cardList = document.querySelector(".review-list");
 const REVIEW_KEY = "reviews";
 const buttonClick = document.getElementById("button-click");
 const savedInfos = localStorage.getItem(REVIEW_KEY);
-let review = [];
+let review = [...JSON.parse(savedInfos)];
 
-buttonClick.addEventListener("click", checking);
 buttonClick.addEventListener("click", play);
 
 // 유효성 검사
@@ -118,10 +153,12 @@ function checking() {
   if (inputName.value === "") {
     alert("작성자명이 비었습니다!");
     return;
-  } else if (inputPassword.value === "") {
+  }
+  if (inputPassword.value === "") {
     alert("비밀번호를 입력해주세요");
     return;
-  } else if (inputContent.value === "") {
+  }
+  if (inputContent.value === "") {
     alert("내용을 한 글 자 이상 입력해주세요!");
     return;
   }
@@ -134,17 +171,21 @@ function saveInfos(newreview) {
 
 // 저장하기 1
 function play() {
-  const newName = inputName.value;
-  const newContent = inputContent.value;
-  const newPassword = inputPassword.value;
+  checking();
+  const newName = inputName;
+  const newContent = inputContent;
+  const newPassword = inputPassword;
   const newInfoObj = {
-    name: newName,
-    content: newContent,
-    password: newPassword,
+    name: newName.value,
+    content: newContent.value,
+    password: newPassword.value,
     movieId,
     id: Date.now(),
   };
-  console.log(review);
+  newName.value = "";
+  newContent.value = "";
+  newPassword.value = "";
+
   review.push(newInfoObj);
   saveInfos(review);
   render(newInfoObj);
@@ -156,6 +197,7 @@ function deleteInfos(event) {
   let newPassword = prompt();
 
   const parentDiv = event.target.parentElement;
+  console.log(parentDiv);
 
   if (newPassword !== parentDiv.dataset.password) {
     alert("비밀번호가 다릅니다.");
@@ -175,7 +217,7 @@ function deleteInfos(event) {
 
 function render() {
   cardList.innerHTML = "";
-  const reviews = localStorage.getItem("reviews");
+  const reviews = localStorage.getItem("reviews") ?? "[]";
 
   JSON.parse(reviews)
     .filter((item) => {
@@ -183,28 +225,24 @@ function render() {
     })
     .forEach((newInfoObj) => {
       const parentDiv = document.createElement("div");
+
       parentDiv.id = newInfoObj.id;
       parentDiv.dataset.password = newInfoObj.password;
-      const divName = document.createElement("div");
-      divName.innerText = `이름 : ${newInfoObj.name}`;
-      const divContent = document.createElement("div");
-      divContent.innerText = `한줄평 : ${newInfoObj.content}`;
-      const deleteButton = document.createElement("button");
-      deleteButton.innerText = "삭제";
-      const modifyButton = document.createElement("button");
-      modifyButton.innerText = "수정";
-      deleteButton.addEventListener("click", deleteInfos);
+      parentDiv.classList.add("review-content__wrapper");
+      parentDiv.innerHTML = `
+        <div class='review-content'>이름 : ${newInfoObj.name}</div>
+        <div class='review-content'>한줄평 : ${newInfoObj.content}</div>
+        <div class="review-btn-group" id="${newInfoObj.id}" data-password="${newInfoObj.password}">
+        <button>삭제</button><!--<button>수정</button>-->
+        </div>
+      `;
+
+      parentDiv.querySelectorAll("button").forEach((btn) => {
+        btn.addEventListener("click", deleteInfos);
+      });
       // modifyButton.addEventListener("click", modifyInfos);
-      parentDiv.appendChild(divName);
-      parentDiv.appendChild(divContent);
-      parentDiv.appendChild(deleteButton);
-      parentDiv.appendChild(modifyButton);
       cardList.appendChild(parentDiv);
     });
 }
 
-if (savedInfos !== null) {
-  const parsedReviews = JSON.parse(savedInfos);
-  inFos = parsedReviews;
-  parsedReviews.forEach(render);
-}
+render();
